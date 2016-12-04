4c505e615ecb129033c92fdb6312fee893759aba
senghuot <senghuotlim@gmail.com>
2016-12-03 18:16:42 -0800

    adding buttons and some functions

--- Lecture1/Calculator/Calculator/ViewController.swift
+++ Lecture1/Calculator/Calculator/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var display: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func appendDigit(_ sender: UIButton) {
+        display!.text = sender.currentTitle!;
     }
-
 
 }
 
