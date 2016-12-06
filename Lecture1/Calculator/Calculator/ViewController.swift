74e84495fab685d571c270b0cfc664e1ee43f3a8
senghuot <senghuotlim@gmail.com>
2016-12-05 23:51:10 -0800

    adding more code

--- Lecture1/Calculator/Calculator/ViewController.swift
+++ Lecture1/Calculator/Calculator/ViewController.swift
@@ -24,7 +24,52 @@ class ViewController: UIViewController {
         }
         
     }
-
+    
+    @IBAction func operate(_ sender: UIButton) {
+        equals();
+        
+        let operation = sender.currentTitle!;
+        
+        switch operation {
+        case "×":
+            performOperation(operation: { (op1: Double, op2: Double) -> Double in
+                return op1 * op2
+            });
+        case "÷":
+            performOperation(operation: { (op1: Double, op2: Double) -> Double in
+                return op1 / op2;
+            });
+        case "-":
+            performOperation(operation: { (op1: Double, op2: Double) -> Double in
+                return op1 - op2;
+            });
+        case "+":
+            performOperation(operation: { (op1: Double, op2: Double) -> Double in
+                return op1 + op2;
+            });
+        default:
+            break;
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count > 1) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast());
+            equals();
+        }
+    }
+    
+    var operandStack = Array<Double>();
+    
+    var displayValue: Double {
+        get {
+            return NumberFormatter().number(from: display.text!)!.doubleValue;
+        }
+        set {
+            display.text = "\(newValue)";
+        }
+    }
+    
     @IBAction func appendDecimal(_ sender: UIButton) {
         if (!hasDecimalUsed) {
             hasDecimalUsed = true;
@@ -32,10 +77,25 @@ class ViewController: UIViewController {
         }
     }
 
+    @IBAction func invertDigit() {
+        displayValue *= -1;
+        if (!hasDecimalUsed) {
+            let displayString = display!.text!;
+            let decimalIndex = displayString.index(displayString.endIndex, offsetBy: -2);
+            display!.text! = displayString.substring(to: decimalIndex);
+        }
+    }
+    
     @IBAction func clear() {
         display!.text! = "0";
         hasDecimalUsed = false;
         isTyping = false;
+    }
+    
+    @IBAction func equals() {
+        isTyping = false;
+        operandStack.append(displayValue);
+        print("operandStack = \(operandStack)");
     }
 }
 
